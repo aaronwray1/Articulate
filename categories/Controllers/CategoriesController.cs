bd8d81088cae31596540ea33e488ae6d536d1058
aaronwray1 <aaron.wray@avanade.com>
2021-06-09 13:52:47 +0100

    Categories service created

---
+++ categories/Controllers/CategoriesController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace categories.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CategoriesController : ControllerBase
+    {
+        private static readonly string[] categories = new[]
+        {
+            "Object", "Nature", "Random", "Person", "Action", "World"
+        };
+
+        private readonly ILogger<CategoriesController> _logger;
+
+        public CategoriesController(ILogger<CategoriesController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public String Get()
+        {
+            var rng = new Random();
+            return categories[rng.Next(0, 5)];
+        }
+    }
+}
